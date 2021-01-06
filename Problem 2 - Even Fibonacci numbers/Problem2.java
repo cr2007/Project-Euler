cb0687296bd8f67345c5d984ba41c646a61aaee5
Sanjana <sanjanasundar2002@gmail.com>
2021-01-06 20:05:12 +0400

    completed

--- Problem 2 - Even Fibonacci numbers/Problem2.java
+++ Problem 2 - Even Fibonacci numbers/Problem2.java
@@ -7,13 +7,12 @@ public class Problem2 {
 		System.out.print("Enter the limit for the array");
 		int limit=scan.nextInt();
 		double[]number=new double[limit];
-		int j=0;
 		number[0]=1;
 		number[1]=2;
 		System.out.println(number[0]);
 		System.out.println(number[1]);	
 		for(int i=2;i<limit;i++)
-		{ //sum=sum+number[i-1]+number[i];
+		{ 
 			number[i]=number[i-1]+number[i-2];
 		System.out.println(number[i]);
 		}
@@ -21,7 +20,7 @@ public class Problem2 {
 		for(int i=0;i<limit;i++)
 		{if(number[i]%2==0)
 		sum=sum+number[i];}
-		System.out.println(sum);
+		System.out.println("The sum is:"+sum);
     }
 
 }
