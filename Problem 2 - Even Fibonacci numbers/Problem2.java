c312a6f9ae39ac16315f43afc21f827b6cec0799
Sanjana <sanjanasundar2002@gmail.com>
2021-01-06 20:32:17 +0400

    completed with comments

--- Problem 2 - Even Fibonacci numbers/Problem2.java
+++ Problem 2 - Even Fibonacci numbers/Problem2.java
@@ -2,24 +2,33 @@ import java.util.Scanner;
 public class Problem2 {
 
 	public static void main(String[] args) {
-    //1,2,3,5,8
+    //1,2,3,5,8....
+		// scanner used to read the limit value from user
 		Scanner scan=new Scanner(System.in);
 		System.out.print("Enter the limit for the array");
 		int limit=scan.nextInt();
 		double[]number=new double[limit];
+		
+		//the first two array elements are added
 		number[0]=1;
 		number[1]=2;
+		System.out.println("The elements of the fibonacci series are");
 		System.out.println(number[0]);
 		System.out.println(number[1]);	
+		
+		//for loop to add elements to the array
 		for(int i=2;i<limit;i++)
 		{ 
 			number[i]=number[i-1]+number[i-2];
 		System.out.println(number[i]);
 		}
 		double sum=0;
+		//read the array
 		for(int i=0;i<limit;i++)
-		{if(number[i]%2==0)
-		sum=sum+number[i];}
+		{
+		if(number[i]%2==0) // condition to check if it is even
+		sum=sum+number[i];
+		}
 		System.out.println("The sum is:"+sum);
     }
 
