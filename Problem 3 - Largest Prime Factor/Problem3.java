41355de1762de6233e9b7a910880d33cf2a8d257
cr2007 <73425927+cr2007@users.noreply.github.com>
2021-01-06 16:00:34 +0400

    Solved 👍

--- Problem 3 - Largest Prime Factor/Problem3.java
+++ Problem 3 - Largest Prime Factor/Problem3.java
@@ -1,7 +1,28 @@
 public class Problem3 {
 
 	public static void main(String[] args) {
-    
-    }
+		long number = Long.parseUnsignedLong("600851475143");
+		System.out.println("The largest Prime Factor of " + number + " is: " + largestPrimeFactor(number));
 
+	}
+
+	public static long largestPrimeFactor(long n) {
+		//Number Check
+		if (n <= 1) {
+			return 0;
+		} else {
+			int div = 2;
+			while (div < n/2) {
+				//Factor Check
+				if (n % div != 0) {
+					div++;
+				//Prime Check
+				} else {
+					n = n / div;
+					div = 2;
+				}
+			}
+			return n;
+		}
+	}
 }
