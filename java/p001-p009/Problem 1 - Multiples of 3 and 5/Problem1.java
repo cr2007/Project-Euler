54c94abc360ba6669eb5ff447cc5b12172138b38
Chandrashekhar R <cr2007@hw.ac.uk>
2021-12-17 10:21:21 +0400

    Adds appropriate commenting

--- java/p001-p009/Problem 1 - Multiples of 3 and 5/Problem1.java
+++ java/p001-p009/Problem 1 - Multiples of 3 and 5/Problem1.java
@@ -1,12 +1,18 @@
 public class Problem1 {
 
 	public static void main(String[] args) {
+		// Creates a variable to store the sum value
 		int multiples = 0;
+
+		/* For loop to calculate the multiples
+		   and add them to the final variable */
 		for(int i=1; i<1000; i++) {
+			/* Condition which checks if the
+			   number is a multiple of 3 or 5 */
 			if((i % 3 == 0) || (i % 5 == 0))
-				multiples += i;
+				multiples += i; // Adds the respective number to the final variable
 		}
-		System.out.println(multiples);
+		System.out.println(multiples); // Prints out the final value
 
 	}
 
