8015412bfc809a6b5dd31563aa8d53d987fdad10
Chandrashekhar R <cr2007@hw.ac.uk>
2022-01-19 15:48:19 +0400

    Adds commenting

--- c/p001-p009/Problem 1 - Multiples of 3 and 5/Problem1.c
+++ c/p001-p009/Problem 1 - Multiples of 3 and 5/Problem1.c
@@ -1,14 +1,21 @@
 #include <stdio.h>
 
+// Main method
 int main(int argc, char const *argv[]) {
-    int multiples = 0;
+    // Declaring and Assigning the variable
+    int multiples;
     multiples = 0;
 
+    /* For Loop to go through the numbers and
+       add the numbers which are factors of 3 or 5 */
     for (int i = 1; i < 1000; i++) {
+        // Condition to check if the number is a factor of 3 or 5
         if ((i % 3 == 0) || (i % 5 == 0)) {
+            // Adds the number to the variable
             multiples += i;
         }
     }
 
+    // Prints out the result
     printf("%d \n", multiples);
 }
