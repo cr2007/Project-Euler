3f945f501bb9c684a99fc567ba952a544d50167f
Chandrashekhar R <cr2007@hw.ac.uk>
2022-04-06 10:47:55 +0400

    Minor Formatting Edits

--- java/p001-p009/Problem 1 - Multiples of 3 and 5/Problem1.java
+++ java/p001-p009/Problem 1 - Multiples of 3 and 5/Problem1.java
@@ -6,12 +6,13 @@ public class Problem1 {
 
 		/* For loop to calculate the multiples
 		   and add them to the final variable */
-		for(int i=1; i<1000; i++) {
+		for(int i = 1; i < 1000; i++) {
 			/* Condition which checks if the
 			   number is a multiple of 3 or 5 */
 			if((i % 3 == 0) || (i % 5 == 0))
 				multiples += i; // Adds the respective number to the final variable
 		}
+		
 		System.out.println(multiples); // Prints out the final value
 
 	}
--- java/p001-p009/Problem 2 - Even Fibonacci numbers/Problem2.java
+++ java/p001-p009/Problem 2 - Even Fibonacci numbers/Problem2.java
@@ -1,36 +1,38 @@
 import java.util.Scanner;
+
 public class Problem2 {
 
 	public static void main(String[] args) {
-		//1,2,3,5,8....
+		// 1,2,3,5,8....
 		// scanner used to read the limit value from user
 		Scanner scan = new Scanner(System.in);
 
-		System.out.print("Enter the limit for the array"); //Prompt to make the user enter the value
-		int limit=scan.nextInt(); //Integer Variable declared based on the user's input
+		System.out.print("Enter the limit for the array"); // Prompt to make the user enter the value
+		int limit = scan.nextInt();                        // Integer Variable declared based on the user's input
 
-		double[] number=new double[limit]; //Array creation based on the 'limit' variable
+		double[] number = new double[limit]; // Array creation based on the 'limit' variable
 
-		//The first two array elements are added
-		number[0]=1;
-		number[1]=2;
+		// The first two array elements are added
+		number[0] = 1;
+		number[1] = 2;
 		System.out.println("The elements of the fibonacci series are");
 		System.out.println(number[0]);
 		System.out.println(number[1]);
 
-		//For loop to add elements to the array
-		for(int i=2;i<limit;i++){
-			number[i]=number[i-1]+number[i-2];
+		// For loop to add elements to the array
+		for (int i = 2; i < limit; i++) {
+			number[i] = number[i - 1] + number[i - 2];
 			System.out.println(number[i]);
 		}
-		double sum=0;
 
-		//read the array
-		for(int i=0;i<limit;i++){
-			if(number[i]%2==0) // condition to check if it is even
-				sum=sum+number[i];
+		double sum = 0;
+
+		// read the array
+		for (int i = 0; i < limit; i++) {
+			if (number[i] % 2 == 0) // condition to check if it is even
+				sum = sum + number[i];
 		}
 
-		System.out.println("The sum is: "+ sum); //Prints out the Sum
+		System.out.println("The sum is: " + sum); // Prints out the Sum
 	}
 }
